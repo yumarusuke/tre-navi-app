b9f737cd25ab51db4b2d7d0ae635d3a2e6a27f2a
“yumarusuke” <yubaseballfunya@gmail.com>
2024-07-25 18:44:37 +0900

    “yumaru”

--- app.py
+++ app.py
@@ -3,11 +3,19 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route("/")
-def top():
-    return render_template("top.html")
+def create():
+    return render_template("create.html")
 
 @app.route("/search")
 def search():
     return render_template("search.html")
+
+@app.route("/map")
+def map():
+    return render_template("map.html")
+
+@app.route("/survey")
+def survey():
+    return render_template("survey.html")
 
 app.run(host="0.0.0.0")
