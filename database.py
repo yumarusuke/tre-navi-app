8dac4420580957a86f79f5c3bc955ee480d78877
“yumarusuke” <yubaseballfunya@gmail.com>
2024-09-05 17:59:32 +0900

    “yuumaru”

--- database.py
+++ database.py
@@ -14,6 +14,12 @@ Person.create(name="ゆうや", age=11)
 class Traveler(Model):
     name = CharField()
     age = IntegerField()
+    from_area = CharField()
+    allergy = CharField()
+    dislike = CharField()
+    interest = CharField()
+    from_date = IntegerField()
+    to_date = IntegerField()
 
     class Meta:
         database = db # This model uses the "people.db" database.
