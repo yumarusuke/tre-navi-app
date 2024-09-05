83831384e75348aeb13ffaf89b0d258d493407a9
“yumarusuke” <yubaseballfunya@gmail.com>
2024-09-05 17:29:41 +0900

    “yuumaru”

--- database.py
+++ database.py
@@ -10,3 +10,12 @@ class Person(Model):
         database = db # This model uses the "people.db" database.
 db.create_tables([Person])
 Person.create(name="ゆうや", age=11)
+
+class Traveler(Model):
+    name = CharField()
+    age = IntegerField()
+
+    class Meta:
+        database = db # This model uses the "people.db" database.
+db.create_tables([Person])
+Person.create(name="ゆうや", age=11)
