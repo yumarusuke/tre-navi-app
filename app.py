3d1fbaee9893844d059de3c1fe075deb30c8ee24
“yumarusuke” <yubaseballfunya@gmail.com>
2024-09-05 17:49:11 +0900

    “yuumaru”

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 from flask import Flask
 from flask import render_template
+from flask import request
+from flask import redirect
+
+from database import Traveler
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -17,5 +22,13 @@ def map():
 @app.route("/survey")
 def survey():
     return render_template("survey.html")
+
+@app.route("/create", methods=["POST"])
+def new_taveler():
+    nickname = request.form["nickname"]
+    age = request.form["age"]
+    Traveler.create(name=nickname, age=age)
+    return redirect("/")
+
 
 app.run(host="0.0.0.0")
